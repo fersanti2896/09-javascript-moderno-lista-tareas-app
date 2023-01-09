0bd69644df4b7b808dcef90d15edda75affa2ba8
Fernando Nicolás <fersanti2896@gmail.com>
2023-01-09 14:32:41 -0600

    Implementando métodos de todo en todo.store.js

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -19,6 +19,81 @@ const initStore = () => {
     console.log('InitStore');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {Filters} filter 
+ */
+const getTodos = ( filter = Filters.All ) => {
+    switch ( filter ) {
+        case Filters.All:
+            return [...state.todos];
+        
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done );
+
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done );
+    
+        default:
+            throw new Error(`Option ${ filter } is not valid`);
+            break;
+    }
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    if ( !description ) throw new Error('Description is required');
+
+    state.todos.push( new Todo(description) );
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const toggleTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId 
+ */
+const deleteTodo = ( todoId ) => {
+    state.todos = state.todos.filter( todo => todo.id !== todoId );
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter( todo => todo.done );
+}
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = ( newFilter = Filters.All ) => {
+    state.filter = newFilter; 
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
 export default {
-    initStore
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
+    initStore,
+    loadStore,
+    setFilter,
+    toggleTodo
 }
